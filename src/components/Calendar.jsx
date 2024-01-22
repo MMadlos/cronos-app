eac1788527edcf0e370eb4ae063feee1f23e6254
michael <michaelmadlos@hotmail.com>
2024-01-22 16:31:22 +0100

    Add dynamic content for Header

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,48 @@
-import { getCurrentCalendar } from "../utils"
+import { useState } from "react"
+
+import { getCalendar } from "../utils"
 
 import CalendarHeader from "./CalendarHeader"
 import CalendarBody from "./CalendarBody"
 
+const now = new Date()
+const currentYear = now.getFullYear()
+const currentMonthIndex = now.getMonth()
+
 function Calendar() {
-	const currentCalendar = getCurrentCalendar()
-	const { currentYear, currentMonth, currentMaxDays, firstWeekDay } = currentCalendar
+	const currentMonth = getCalendar(currentMonthIndex, currentYear)
+
+	const data = {
+		year: currentYear,
+		monthIndex: currentMonthIndex,
+		month: currentMonth.month,
+		maxDays: currentMonth.maxDays,
+		firstWeekDay: currentMonth.firstWeekDay,
+	}
+
+	// State: cuando el state cambie, los parámetros de CalendarHeader y CalendarBody deben cambiar
+	const [calendar, setCalendar] = useState(data)
+	const { year, maxDays, firstWeekDay } = calendar
+
+	function handleNextMonth(e) {
+		const currentMonthIndexEl = e.target.closest("[data-month-index]")
+		const currentMonthIndex = Number(currentMonthIndexEl.dataset.monthIndex)
+
+		const typeMonthEl = e.target.closest("[data-type]")
+		const typeMonth = typeMonthEl.dataset.type
+
+		const newMonthIndex = typeMonth === "next" ? currentMonthIndex + 1 : currentMonthIndex - 1
+
+		const getNewCalendar = getCalendar(newMonthIndex, year)
+		const newCalendar = {
+			year,
+			monthIndex: newMonthIndex,
+			month: getNewCalendar.month,
+			maxDays: getNewCalendar.maxDays,
+			firstWeekDay: getNewCalendar.firstWeekDay,
+		}
+		setCalendar(newCalendar)
+	}
 
 	return (
 		<div className="my-8 flex flex-col gap-4">
@@ -13,11 +50,11 @@ function Calendar() {
 				id="calendar"
 				className="w-[800px]  bg-indigo-50 p-8 flex flex-col gap-6 rounded-xl">
 				<CalendarHeader
-					month={currentMonth}
-					currentYear={currentYear}
+					calendarData={calendar}
+					onClick={handleNextMonth}
 				/>
 				<CalendarBody
-					daysOfMonth={currentMaxDays}
+					daysOfMonth={maxDays}
 					startsOn={firstWeekDay}
 				/>
 			</div>
--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,14 +1,25 @@
-function CalendarHeader({ year, month }) {
+function CalendarHeader({ calendarData, onClick }) {
+	const { year, month, monthIndex } = calendarData
+
 	return (
 		<div
 			id="month-selector"
+			data-month-index={monthIndex}
 			className="flex flex-row justify-between items-center ">
-			<i className="fa-solid fa-arrow-left text-3xl text-indigo-500" />
+			<button
+				onClick={onClick}
+				data-type="previous">
+				<i className="fa-solid fa-arrow-left text-3xl text-indigo-500" />
+			</button>
 			<div className="flex flex-col items-center justify-center">
 				<span className="text-indigo-300 text-sm font-medium">{year}</span>
 				<p className="text-3xl font-semibold text-indigo-700">{month}</p>
 			</div>
-			<i className="fa-solid fa-arrow-right text-3xl text-indigo-500" />
+			<button
+				onClick={onClick}
+				data-type="next">
+				<i className="fa-solid fa-arrow-right text-3xl text-indigo-500" />
+			</button>
 		</div>
 	)
 }
