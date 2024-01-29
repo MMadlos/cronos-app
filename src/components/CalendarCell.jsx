8a84dd5b336b6da72d22f60214f6c1819c25b183
MMadlos <michaelmadlos@gmail.com>
2024-01-29 16:21:59 +0100

    Add correct styles

--- src/components/CalendarCell.jsx
+++ src/components/CalendarCell.jsx
@@ -6,35 +6,37 @@ function CalendarCell({ date, onClickDate, monthIndex, filteredDates }) {
 
 	const isSelected = filteredDates.includes(date)
 
+	const isToday =
+		date === dateToday.getDate() && monthIndex === dateToday.getMonth()
+
 	const classes = {
 		base: "flex justify-center rounded-md font-medium items-center size-12 border-2",
 		isEmpty: "bg-zinc-100 border-zinc-100",
 		notAvailable: "bg-zinc-100 border-zinc-100 text-zinc-300",
 		isToday: "bg-zinc-50 border-zinc-500 text-zinc-700 hover:bg-zinc-200",
-		isSelected:
-			"bg-zinc-400 border-zinc-400 text-zinc-500 hover:cursor-pointer",
-		default:
-			"bg-zinc-50 border-zinc-200 text-zinc-600 hover:bg-zinc-200 hover:cursor-pointer",
+		isSelected: "bg-zinc-900 border-zinc-900 text-zinc-50 font-medium",
+		default: "bg-zinc-50 border-zinc-200 text-zinc-600 hover:bg-zinc-200",
 	}
 
 	const getClasses = () => {
 		if (date === undefined) return classes.isEmpty
 		if (isPastDate) return classes.notAvailable
 		if (isSelected) return classes.isSelected
+		if (isToday) return classes.isToday
 		return classes.default
 	}
 
 	const classType = getClasses()
 
 	return (
-		<div
+		<button
 			className={`${classes.base} ${classType}`}
 			data-date={date}
 			onClick={onClickDate}
 			data-selected={isSelected}
 		>
 			{date}
-		</div>
+		</button>
 	)
 }
 
