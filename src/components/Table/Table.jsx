35decb4a9a70d354abc3adb89d6d6fe57178a0fe
MMadlos <michaelmadlos@gmail.com>
2024-02-06 17:25:51 +0100

    Move table head to its component

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,63 +1,13 @@
-import { getIntlMonthLong, getIntlWeekdayShort } from "../../utils"
-
 import TableHead from "./TableHead"
 import TableBody from "./TableBody"
 
 export default function Table({ selectedDates, participants }) {
 	// 8. El total de filas = 1 (mes) + 1 (días) + num total de participantes
 
-	const sortedByMonth = {}
-	selectedDates.forEach((object) => {
-		const month = getIntlMonthLong(object)
-		const weekday = getIntlWeekdayShort(object)
-		const date = object.getDate()
-
-		if (sortedByMonth[month] === undefined) sortedByMonth[month] = []
-		sortedByMonth[month].push({ weekday, date })
-	})
-
-	const tableCount = {
-		monthCount: Object.keys(sortedByMonth).length,
-	}
-
-	for (let month in sortedByMonth) {
-		tableCount[month] = sortedByMonth[month].length
-	}
-
-	const allMonths = Object.keys(sortedByMonth)
-	const allDates = Object.values(sortedByMonth).flat()
-
 	return (
 		<div className=": w-full overflow-x-auto  bg-zinc-200 md:max-w-[1200px] md:rounded-xl">
 			<table id="table" className="w-full border-collapse bg-zinc-50 ">
-				<thead>
-					<tr>
-						<th id="empty-th"></th>
-						{allMonths.map((month, index) => {
-							const colSpan = tableCount[month]
-
-							return (
-								<th key={index} id={index} colSpan={colSpan}>
-									{month}
-								</th>
-							)
-						})}
-						<th></th>
-					</tr>
-					<tr>
-						<th id="participants">Participantes</th>
-						{allDates.map((dates, index) => {
-							const { weekday, date } = dates
-
-							return (
-								<th key={index} id={index}>
-									{`${weekday} ${date}`}
-								</th>
-							)
-						})}
-						<th></th>
-					</tr>
-				</thead>
+				<TableHead selectedDates={selectedDates} />
 				<TableBody
 					peopleList={participants}
 					selectedDates={selectedDates}
@@ -66,11 +16,6 @@ export default function Table({ selectedDates, participants }) {
 		</div>
 	)
 }
-
-// BACKUP
-/*
-<TableHead selectedDates={selectedDates} />
-*/
 
 // REMOVED TEMPORARY
 /*
--- src/components/Table/TableHead.jsx
+++ src/components/Table/TableHead.jsx
@@ -1,21 +1,54 @@
-import { getFormattedDates } from "../../utils"
+import { getIntlMonthLong, getIntlWeekdayShort } from "../../utils"
 
 export default function TableHead({ selectedDates }) {
-	const formattedDates = getFormattedDates(selectedDates)
+	const sortedByMonth = {}
+	selectedDates.forEach((object) => {
+		const month = getIntlMonthLong(object)
+		const weekday = getIntlWeekdayShort(object)
+		const date = object.getDate()
+
+		if (sortedByMonth[month] === undefined) sortedByMonth[month] = []
+		sortedByMonth[month].push({ weekday, date })
+	})
+
+	const tableCount = {
+		monthCount: Object.keys(sortedByMonth).length,
+	}
+
+	for (let month in sortedByMonth) {
+		tableCount[month] = sortedByMonth[month].length
+	}
+
+	const allMonths = Object.keys(sortedByMonth)
+	const allDates = Object.values(sortedByMonth).flat()
 
 	return (
 		<thead>
 			<tr>
-				<th id="participants"></th>
-				{formattedDates.map((dates, index) => {
-					const dateTime = selectedDates[index].getTime()
+				<th id="empty-th"></th>
+				{allMonths.map((month, index) => {
+					const colSpan = tableCount[month]
 
 					return (
-						<th key={dateTime} id={dateTime}>
-							{dates}
+						<th key={index} id={index} colSpan={colSpan}>
+							{month}
 						</th>
 					)
 				})}
+				<th></th>
+			</tr>
+			<tr>
+				<th id="participants">Participantes</th>
+				{allDates.map((dates, index) => {
+					const { weekday, date } = dates
+
+					return (
+						<th key={index} id={index}>
+							{`${weekday} ${date}`}
+						</th>
+					)
+				})}
+				<th></th>
 			</tr>
 		</thead>
 	)
