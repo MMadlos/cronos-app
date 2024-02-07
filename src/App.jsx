b5d081376aa3c8461f2f36b87d49d0d0dc940fb7
MMadlos <michaelmadlos@gmail.com>
2024-02-07 12:45:16 +0100

    Add image header and info section

--- src/App.jsx
+++ src/App.jsx
@@ -143,72 +143,98 @@ function App() {
 	}
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-indigo-100">
-			{stage === stages[0] && (
-				<div>
-					<Calendar
-						calendarData={calendarData}
-						selectedDays={selectedDays}
-						onClickArrows={handleMonthArrows}
-						onClickDate={handleSelectDays}
-						onClickWeekday={handleSelectWeek}
-					/>
-					<div className="mt-8 flex w-full flex-row justify-between">
-						<button
-							className="rounded-lg bg-zinc-50 px-6 py-2 font-semibold text-red-600 hover:opacity-50"
-							onClick={() => setSelectedDays([])}
-						>
-							{selectedDays.length === 0
-								? "Select days"
-								: "Reset"}
-						</button>
-						<button
-							className="rounded-lg bg-zinc-900 px-6 py-2 font-semibold text-zinc-50 hover:opacity-50"
-							onClick={() => {
-								if (selectedDays.length === 0) return
-								setStage(stages[1])
-							}}
-						>
-							Done
-						</button>
-					</div>
-				</div>
-			)}
-
-			{stage === stages[1] && (
-				<UserList
-					participants={participants}
-					inputValue={inputValue}
-					onChangeInput={handleChangeInput}
-					onKeyDownEnter={handleAddParticipants}
-					onClickAdd={handleAddParticipants}
-					onClickRemove={handleClickRemove}
-					onClickNext={() => setStage(stages[2])}
-					onClickReturn={() => setStage(stages[0])}
+		<div className="container mx-auto min-h-dvh min-w-[300px] max-w-[800px] bg-zinc-50 px-8">
+			<header className="py-8">
+				<img
+					src="src/images/birthday-md.jpg"
+					alt="an image with party elements"
+					className="h-[250px] w-full rounded-lg object-cover object-top"
 				/>
-			)}
-
-			{stage === stages[2] && (
-				<div className="h-dvh max-w-[80dvw] bg-slate-50 px-20 pt-10">
-					<div className="flex flex-row items-center justify-between pb-2">
-						<div className="flex flex-row items-center gap-4 ">
-							<h1 className="text-left text-3xl font-bold text-zinc-600">
-								Título
-							</h1>
-							<i className="fa-solid fa-pen text-zinc-300 hover:cursor-pointer hover:text-zinc-400" />
-						</div>
-						<button className="rounded-md border border-zinc-300 bg-zinc-100 px-10 py-2 font-semibold text-zinc-500 hover:bg-zinc-200 hover:text-zinc-600">
-							Edit
-						</button>
-					</div>
-					<div className="mb-8 border-b-4 border-zinc-300"></div>
-					<Table
-						participants={participants}
-						selectedDates={selectedDays}
-					/>
+			</header>
+			<section>
+				<div className="flex flex-row items-center gap-4">
+					<h1 className=" text-2xl font-semibold text-zinc-700">
+						Cumpleaños Mari Pili
+					</h1>
+					<i className="fa-solid fa-pen cursor-pointer rounded p-2 text-zinc-400 hover:bg-zinc-100 hover:text-zinc-500" />
 				</div>
-			)}
-		</main>
+				<div className="mt-4 rounded-lg bg-white p-4 shadow-sm">
+					<p className="leading-relaxed text-zinc-800">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Distinctio molestiae minus quaerat aliquam ex?
+						Exercitationem incidunt, ducimus molestias perspiciatis
+						perferendis nisi voluptatibus veniam dolorum sapiente,
+						quae voluptatem magni eius provident.
+					</p>
+				</div>
+			</section>
+			<main className="flex  flex-col items-center justify-center bg-indigo-100">
+				{stage === stages[0] && (
+					<div>
+						<Calendar
+							calendarData={calendarData}
+							selectedDays={selectedDays}
+							onClickArrows={handleMonthArrows}
+							onClickDate={handleSelectDays}
+							onClickWeekday={handleSelectWeek}
+						/>
+						<div className="mt-8 flex w-full flex-row justify-between">
+							<button
+								className="rounded-lg bg-zinc-50 px-6 py-2 font-semibold text-red-600 hover:opacity-50"
+								onClick={() => setSelectedDays([])}
+							>
+								{selectedDays.length === 0
+									? "Select days"
+									: "Reset"}
+							</button>
+							<button
+								className="rounded-lg bg-zinc-900 px-6 py-2 font-semibold text-zinc-50 hover:opacity-50"
+								onClick={() => {
+									if (selectedDays.length === 0) return
+									setStage(stages[1])
+								}}
+							>
+								Done
+							</button>
+						</div>
+					</div>
+				)}
+
+				{stage === stages[1] && (
+					<UserList
+						participants={participants}
+						inputValue={inputValue}
+						onChangeInput={handleChangeInput}
+						onKeyDownEnter={handleAddParticipants}
+						onClickAdd={handleAddParticipants}
+						onClickRemove={handleClickRemove}
+						onClickNext={() => setStage(stages[2])}
+						onClickReturn={() => setStage(stages[0])}
+					/>
+				)}
+
+				{stage === stages[2] && (
+					<div className="h-dvh max-w-[80dvw] bg-slate-50 px-20 pt-10">
+						<div className="flex flex-row items-center justify-between pb-2">
+							<div className="flex flex-row items-center gap-4 ">
+								<h1 className="text-left text-3xl font-bold text-zinc-600">
+									Título
+								</h1>
+								<i className="fa-solid fa-pen text-zinc-300 hover:cursor-pointer hover:text-zinc-400" />
+							</div>
+							<button className="rounded-md border border-zinc-300 bg-zinc-100 px-10 py-2 font-semibold text-zinc-500 hover:bg-zinc-200 hover:text-zinc-600">
+								Edit
+							</button>
+						</div>
+						<div className="mb-8 border-b-4 border-zinc-300"></div>
+						<Table
+							participants={participants}
+							selectedDates={selectedDays}
+						/>
+					</div>
+				)}
+			</main>
+		</div>
 	)
 }
 
