55d6520af92ab3edcbbe9283da9df4e23583a8c1
MMadlos <michaelmadlos@gmail.com>
2024-02-14 11:59:25 +0100

    Fix style when dataType is part

--- src/components/SummaryCalendar/SumCalCell.jsx
+++ src/components/SummaryCalendar/SumCalCell.jsx
@@ -1,10 +1,12 @@
 export default function SumCalCell({ content, dataType }) {
+	// dataType = weekday | full | part | none | ns
+
 	return (
 		<div
 			className="group flex size-10 items-center justify-center"
 			data-type={dataType}
 		>
-			<div className="flex size-8 items-center justify-center rounded-full group-data-[type=full]:bg-green-300 group-data-[type=none]:bg-zinc-200 group-data-[type=partial]:bg-yellow-500">
+			<div className="flex size-8 items-center justify-center rounded-full group-data-[type=full]:bg-green-300 group-data-[type=none]:bg-zinc-200 group-data-[type=part]:bg-yellow-500">
 				<span className="font-semibold text-zinc-800 group-data-[type=ns]:font-normal group-data-[type=weekday]:font-normal group-data-[type=ns]:text-zinc-300 group-data-[type=weekday]:text-zinc-500">
 					{content}
 				</span>
