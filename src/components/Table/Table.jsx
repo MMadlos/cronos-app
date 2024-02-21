413a532b757a4390685d07a5c37d54d8de7f5eeb
MMadlos <michaelmadlos@gmail.com>
2024-02-21 11:28:42 +0100

    Add formatted dates into Date() instead of time format

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,13 +2,15 @@ import TableHead from "./TableHead"
 import TableBody from "./TableBody"
 
 export default function Table({ selectedDates, participants }) {
+	const formattedDates = selectedDates.map((dateTime) => new Date(dateTime))
+
 	return (
 		<div className="mt-6 overflow-x-auto overflow-y-auto">
 			<table id="table" className="border-collapse bg-white">
-				<TableHead selectedDates={selectedDates} />
+				<TableHead selectedDates={formattedDates} />
 				<TableBody
 					peopleList={participants}
-					selectedDates={selectedDates}
+					selectedDates={formattedDates}
 				/>
 			</table>
 		</div>
