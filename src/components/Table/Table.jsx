4aad0630305624b1fbf4851f3e763b7749c268b8
MMadlos <michaelmadlos@gmail.com>
2024-02-22 16:42:50 +0100

    Refactor how table are consuming state

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,15 +2,13 @@ import TableHead from "./TableHead"
 import TableBody from "./TableBody"
 
 export default function Table({ selectedDates, participants }) {
-	const formattedDates = selectedDates.map((dateTime) => new Date(dateTime))
-
 	return (
 		<div className="mt-6 overflow-x-auto overflow-y-auto">
 			<table id="table" className="border-collapse bg-white">
-				<TableHead selectedDates={formattedDates} />
+				<TableHead selectedDates={selectedDates} />
 				<TableBody
 					peopleList={participants}
-					selectedDates={formattedDates}
+					selectedDates={selectedDates}
 				/>
 			</table>
 		</div>
--- src/components/Table/TableBody.jsx
+++ src/components/Table/TableBody.jsx
@@ -1,24 +1,53 @@
+import { useContext, useState, useEffect } from "react"
+import { ConfirmedDataContext } from "../../App"
 import TableData from "./TableData"
 
 export default function TableBody({ peopleList, selectedDates }) {
+	// const { confirmedData, setConfirmedData } = useContext(ConfirmedDataContext)
+	const [confirmed, setConfirmed] = useState([])
+
+	function handleConfirmArray(id, dateTime) {
+		const isIncluded =
+			confirmed.find(
+				(data) => data.id === id && data.dateTime === dateTime
+			) !== undefined
+
+		if (!isIncluded) setConfirmed((prev) => [...prev, { id, dateTime }])
+		if (isIncluded) {
+			const dataToRemove = confirmed.find(
+				(data) => data.id === id && data.dateTime === dateTime
+			)
+			const newConfirmedArray = confirmed.filter(
+				(data) => data !== dataToRemove
+			)
+			setConfirmed(newConfirmedArray)
+		}
+	}
+
 	return (
 		<tbody>
-			{peopleList.map((peopleData) => {
-				const { id, name } = peopleData
-
+			{peopleList.map(({ id, name }) => {
 				return (
 					<tr key={id}>
 						<th id={id} headers="participants">
 							{name}
 						</th>
-						{selectedDates.map((dates) => {
-							const dateTime = dates.getTime()
+						{selectedDates.map((dateTime) => {
+							const isSelected =
+								confirmed.find(
+									(data) =>
+										data.id === id &&
+										data.dateTime === dateTime
+								) !== undefined
 
 							return (
 								<TableData
-									key={dateTime}
-									dateTime={dateTime}
-									participantID={id}
+									key={`${id}-${dateTime}`}
+									headers={`${dateTime} ${id}`}
+									isSelected={isSelected}
+									onClick={() => {
+										handleConfirmArray(id, dateTime)
+									}}
 								/>
 							)
 						})}
