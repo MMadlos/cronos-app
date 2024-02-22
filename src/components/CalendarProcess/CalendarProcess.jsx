ac0fbcd89fc15ee52421b396b5d7682841a8f8d6
MMadlos <michaelmadlos@gmail.com>
2024-02-22 19:49:41 +0100

    Remove unused component

--- src/components/CalendarProcess/CalendarProcess.jsx
+++
@@ -1,9 +0,0 @@
-export default function CalendarProcess({ children }) {
-	return (
-		<div className="flex h-full items-center justify-center">
-			<div className=" flex h-[90%]  w-[70%] items-center justify-center rounded-lg bg-zinc-100 p-6">
-				{children}
-			</div>
-		</div>
-	)
-}
