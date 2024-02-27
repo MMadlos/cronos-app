0e6de399f69282ebbfecd521c19677d8385cbfd3
MMadlos <michaelmadlos@gmail.com>
2024-02-27 18:31:54 +0100

    Change content to spanish

--- src/components/InitialSetup/Progress.jsx
+++ src/components/InitialSetup/Progress.jsx
@@ -8,14 +8,14 @@ export default function Progress({ stage }) {
 				1
 			</span>
 			<p className="text-nowrap font-semibold text-violet-700 group-data-[stage=list]:text-zinc-900">
-				Select dates
+				Fechas
 			</p>
 			<div className="h-[2px] w-full bg-zinc-300 sm:w-12"></div>
 			<span className="flex size-5 items-center justify-center rounded-full bg-zinc-200 p-4 font-semibold text-zinc-400 group-data-[stage=list]:bg-violet-700 group-data-[stage=list]:text-violet-50 sm:size-10">
 				2
 			</span>
 			<p className="text-nowrap font-medium text-zinc-400 group-data-[stage=list]:font-semibold group-data-[stage=list]:text-violet-700">
-				Add participants
+				Participantes
 			</p>
 		</div>
 	)
