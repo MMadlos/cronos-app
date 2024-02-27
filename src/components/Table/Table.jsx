069546c37220b751e2f088de96d12193b8dc1b53
MMadlos <michaelmadlos@gmail.com>
2024-02-27 18:20:55 +0100

    Remove comments or unnecessary code

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 
 import TableHead from "./TableHead"
 import TableBody from "./TableBody"
-import TableXS from "./TableXS"
 import ScreenXS from "./ScreenXS"
 
 export default function Table() {
@@ -29,12 +28,11 @@ export default function Table() {
 	return (
 		<>
 			<div className="mt-8 hidden sm:block ">
-				{/* Table SCREEN */}
-				<h2 className="text-md font-semibold text-zinc-600 sm:text-lg">
+				<h2 className="text-lg font-semibold text-zinc-600">
 					Fechas propuestas
 				</h2>
 				<div className="h-[1px] w-full bg-zinc-200"></div>
-				<div className="mt-2 max-h-[70vh] w-full overflow-x-auto overflow-y-auto sm:mt-6  sm:max-h-[60vh]">
+				<div className="mt-6 max-h-[60vh] w-full overflow-x-auto  overflow-y-auto">
 					<table
 						id="table"
 						className="w-full border-collapse bg-white"
--- src/components/Table/TableBody.jsx
+++ src/components/Table/TableBody.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext } from "react"
 import { ParticipantsContext, SelectedDatesContext } from "../../App"
 import TableData from "./TableData"
 
