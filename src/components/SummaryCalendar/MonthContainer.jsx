58708ae16b1be978502474877d14d0bac7786ad1
MMadlos <michaelmadlos@gmail.com>
2024-03-01 12:14:42 +0100

    Remove unused Summary Calendar components

--- src/components/SummaryCalendar/MonthContainer.jsx
+++
@@ -1,8 +0,0 @@
-export default function MonthContainer({ monthName, children, className }) {
-	return (
-		<div className={className}>
-			<h3 className="py-2 font-semibold text-zinc-400">{monthName}</h3>
-			{children}
-		</div>
-	)
-}
